0443c1c2fbf42f639ff9c81dbe0fc59ea1a27a7c
Rajeswaran149 <rajeswaran149@gmail.com>
2022-06-18 14:57:28 +0530

    pizza app

---
+++ database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url = "mongodb+srv://rajeswaran:rajes@149@cluster0.eutso4r.mongodb.net/?retryWrites=true&w=majority";
+
+module.exports.connect = () => {
+    mongoose
+      .connect(url, {
+        useNewUrlParser: true,
+        // useFindAndModify: false,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+      })
+      .then(() => console.log("MongoDB is connected successfully"))
+      .catch((err) => console.log("Error: ", err));
+  };
